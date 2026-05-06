2a550795eb3fd0897f8a32ac2466662235f08680
Obuli Sai Naren <66naren@gmail.com>
2026-05-06 18:44:59 +0530

    chore(format): 🎨 add prettier config
    Add the shared Prettier configuration used by the pnpm workspace and lint-staged formatting hook.

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 120,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+};
